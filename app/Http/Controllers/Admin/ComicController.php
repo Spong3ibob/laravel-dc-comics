6afc4126a5d1be77f5dfd375e6e0254093c16fff
Spong3ibob <leonardomaran17@gmail.com>
2023-01-19 17:37:41 +0100

    creato template

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comics;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -14,7 +15,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comics::all();
+        return view('comic.index', compact('comics'));
     }
 
     /**
@@ -44,9 +46,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comics $comics)
     {
-        //
+       dd($comics);
     }
 
     /**
---
+++ resources/views/comic/index.blade.php
@@ -0,0 +1,42 @@
+@extends('leyauts.main')
+
+@section('page-content')
+    <div class="container">
+        <h1>Lista Comics</h1>
+
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrition</th>
+                <th scope="col">Price</th>
+                <th scope="col">Type</th>
+                <th scope="col">Sale Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+
+
+    </div>
+    
+@endsection
